5d546173e7dd3c850682e4917ccb2de145d3b0a0
Alexander Fürstenau <alex@fuerstenau-clan.de>
2012-11-07 14:09:18 +0100

    added simple validation

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,6 @@
 class Member < ActiveRecord::Base
   attr_accessible :beitrag, :email, :first_name, :last_name, :phone, :postcode, :street, :town
+  validates :beitrag, :first_name, :last_name, :postcode, :street, :town, presence: true
+  validates :beitrag, numericality: {greater_than: 0}
+  validates :postcode, numericality: {greater_than: 0}, numericality: {less_than: 100000}
 end
--- test/unit/member_test.rb
+++ test/unit/member_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class MemberTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :members
+  test "member attributes must not be empty" do
+    member = Member.new
+    assert member.invalid?
+    assert member.errors[:first_name].any?
+  end
+  
+  test "beitrag must be positive" do
+    member = Member.new(beitrag: -1, first_name: "Alex", last_name: "Fuerstenau", street: "Max-Tau-Str. 9", postcode: "22529", town: "Hamburg")
+    assert member.invalid?
+    assert_equal ["must be greater than 0"], member.errors[:beitrag]
+  end
 end
