11be5ac8cf195c5df64eb01c6c10f86827eaff45
Alexander Fürstenau <alex@fuerstenau-clan.de>
2012-11-08 10:51:08 +0100

    Eintrittsdatum and Austrittsdatum added

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,6 +1,6 @@
 class Member < ActiveRecord::Base
   has_many :children, dependent: :destroy
-  attr_accessible :beitrag, :email, :first_name, :last_name, :phone, :postcode, :street, :town
+  attr_accessible :beitrag, :email, :first_name, :last_name, :phone, :postcode, :street, :town, :eintrittsdatum, :austrittsdatum
   validates :beitrag, :first_name, :last_name, :postcode, :street, :town, presence: true
   validates :beitrag, numericality: {greater_than: 0}
   validates :postcode, numericality: {greater_than: 0}, numericality: {less_than: 100000}
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121107194744) do
+ActiveRecord::Schema.define(:version => 20121108094028) do
 
   create_table "children", :force => true do |t|
     t.integer  "member_id"
@@ -29,10 +29,12 @@ ActiveRecord::Schema.define(:version => 20121107194744) do
     t.string   "email"
     t.string   "street"
     t.integer  "postcode"
-    t.decimal  "beitrag",    :precision => 8, :scale => 2
+    t.decimal  "beitrag",        :precision => 8, :scale => 2
     t.string   "town"
-    t.datetime "created_at",                               :null => false
-    t.datetime "updated_at",                               :null => false
+    t.datetime "created_at",                                   :null => false
+    t.datetime "updated_at",                                   :null => false
+    t.date     "eintrittsdatum"
+    t.date     "austrittsdatum"
   end
 
 end
