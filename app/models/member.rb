54804b070e1edb6b39731e33274fb3db5cd13fc9
Alexander Fürstenau <alex@fuerstenau-clan.de>
2012-12-11 23:30:33 +0100

    Kleiner Bugfix und Typo

--- app/models/member.rb
+++ app/models/member.rb
@@ -21,10 +21,17 @@ class Member < ActiveRecord::Base
       csv << ["localBankCode", "localAccountNumber", "localName" \
         ,"remoteBankCode", "remoteAccountNumber", "remoteName" \
         ,"valutaDate", "date", "value_value", "value_currency" \
-        ,"transactionKey", , "transactionCode", "transactionText", "purpose", "purpose1"]
+        ,"transactionKey", "transactionCode", "transactionText", "purpose", "purpose1"]
       all.each do |member|
+        if !member.bank_account.nil? then
+          blz = member.bank_account.blz
+          account_number = member.bank_account.account_number
+        else
+          blz = "Kein Konto"
+          account_number = "Kein Konto"
+        end
         csv << ["20050550", "1259125449", "Grundschule, Schulverein d" \
-        , member.bank_account.blz, member.bank_account.account_number, member.full_name \
+        , blz, account_number, member.full_name \
         , current_date, current_date, member.beitrag.truncate, "EUR" \
         , "MSC", "05", "Lastschrift", "Jahresbeitrag Schulverein 2012/2013", "Vielen Dank!"] 
       end
